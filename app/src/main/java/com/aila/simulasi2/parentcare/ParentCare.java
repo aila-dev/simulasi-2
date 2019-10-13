7eb30ca93bed33cbb6814047c28bd74812803807
renryuuka <vembrivrd@gmail.com>
2019-10-13 20:21:33 +0700

    initialize package

---
+++ app/src/main/java/com/aila/simulasi2/parentcare/ParentCare.java
@@ -0,0 +1,16 @@
+package com.aila.simulasi2.parentcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.simulasi2.R;
+
+public class ParentCare extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_care);
+    }
+}
