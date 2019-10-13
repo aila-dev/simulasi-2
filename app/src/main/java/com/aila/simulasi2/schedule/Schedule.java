71bf64848e89e20c79e639ee5cc82cf666697eb7
renryuuka <vembrivrd@gmail.com>
2019-10-13 20:15:50 +0700

    activity empty

---
+++ app/src/main/java/com/aila/simulasi2/schedule/Schedule.java
@@ -0,0 +1,16 @@
+package com.aila.simulasi2.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.simulasi2.R;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
